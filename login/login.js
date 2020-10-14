2eb230b776afdfb80f6ac643fc7d7782d35b4d2a
lim <1317819306@qq.com>
2020-10-14 13:15:09 +0800

    login的静态页面基本完成

--- login/login.js
+++ login/login.js
@@ -1,9 +1,31 @@
 var login=new Vue({
 	el:"#login",
-	data:{
-		
-	},
-	methods:{
-		
-	}
+ data() {
+      return {
+        numberValidateForm: {
+          user: '',
+		  password:''
+        }
+      };
+    },
+	methods: {
+	      submitForm(formName) {
+	        this.$refs[formName].validate((valid) => {
+	          if (valid) {
+	            // alert('submit!');
+				console.log(this.numberValidateForm.user);
+				console.log(this.numberValidateForm.password);
+				//点击成功
+	          } else {
+	            console.log('error submit!!');
+	            return false;
+	          }
+	        });
+	      },
+	      resetForm(formName) {
+	        this.$refs[formName].resetFields();
+	      }
+	    },
+		pust:function(){
+			}
 })
